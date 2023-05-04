e743ec64ebd4d022644e3e9358c66349dc8f4878
ablaze <15084078791@163.com>
2023-05-04 17:33:11 +0800

    v0.01 create repository

---
+++ springboot_01_01_quickstart/src/main/java/com/ablaze/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.ablaze.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ablaze
+ * @Date: 2023/05/04/17:27
+ */
+@RestController
+@RequestMapping("/books")
+public class BookController {
+}
