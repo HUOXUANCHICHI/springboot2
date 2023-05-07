10b79849265b855d7ed837398cb73943e60d0a18
ablaze <15084078791@163.com>
2023-05-07 15:30:49 +0800

    v0.04master bootmp整合

--- springboot_01_01_quickstart/src/main/java/com/ablaze/Springboot0101QuickstartApplication.java
+++ springboot_01_01_quickstart/src/main/java/com/ablaze/Springboot0101QuickstartApplication.java
@@ -1,13 +1,19 @@
 package com.ablaze;
 
+import com.ablaze.controller.BookController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot0101QuickstartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        BookController bean = ctx.getBean(BookController.class);
+        System.out.println("bean=====>" + bean);
+        User user = ctx.getBean(User.class);
+        System.out.println(user);
     }
 
 }
