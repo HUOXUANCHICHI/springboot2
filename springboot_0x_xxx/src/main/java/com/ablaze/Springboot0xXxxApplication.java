d74cf02a19549a67b6dd84eaa631ed02f72e57b0
ablaze <15084078791@163.com>
2023-05-07 15:29:46 +0800

    v0.04master bootmp整合

---
+++ springboot_0x_xxx/src/main/java/com/ablaze/Springboot0xXxxApplication.java
@@ -0,0 +1,14 @@
+package com.ablaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0xXxxApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Springboot0xXxxApplication.class, args);
+    }
+
+}
