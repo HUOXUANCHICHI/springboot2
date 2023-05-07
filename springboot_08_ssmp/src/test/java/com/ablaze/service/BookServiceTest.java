bdb133d7207527033c51e470c168f5daed5cbc2d
ablaze <15084078791@163.com>
2023-05-07 15:41:45 +0800

    v0.04master bootmp整合

---
+++ springboot_08_ssmp/src/test/java/com/ablaze/service/BookServiceTest.java
@@ -0,0 +1,65 @@
+package com.ablaze.service;
+
+import com.ablaze.domain.Book;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: ablaze
+ * @Date: 2023/05/07/12:48
+ */
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private IBookService bookService;
+
+    @Test
+    void testGetById() {
+        System.out.println(bookService.getById(4));
+    }
+
+    @Test
+    void testSave() {
+        Book book = new Book();
+        book.setType("测试数据123");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookService.save(book);
+    }
+
+    @Test
+    void testUpdate() {
+        Book book = new Book();
+        book.setId(17);
+        book.setType("测试数据abcdefg");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookService.updateById(book);
+    }
+
+    @Test
+    void testDelete() {
+        bookService.removeById(16);
+    }
+
+    @Test
+    void testGetAll() {
+        bookService.list();
+    }
+
+    @Test
+    void testGetPage() {
+        IPage<Book> page = new Page<>(2,5);
+        bookService.page(page);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getSize());
+        System.out.println(page.getTotal());
+        System.out.println(page.getPages());
+        System.out.println(page.getRecords());
+    }
+
+}
