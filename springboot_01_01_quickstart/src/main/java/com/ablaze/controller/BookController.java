9ff87ed6d5582adf084635c6e1b9a628655742ec
ablaze <15084078791@163.com>
2023-05-07 15:26:45 +0800

    v0.03master bootmp整合

--- springboot_01_01_quickstart/src/main/java/com/ablaze/controller/BookController.java
+++ springboot_01_01_quickstart/src/main/java/com/ablaze/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.ablaze.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    @GetMapping
+    public String getById() {
+        System.out.println("springboot is running...");
+        return "springboot is running...";
+    }
 }
